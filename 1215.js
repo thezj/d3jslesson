e62522df13b5cfb4c76d6b79554a916e2c555a2a
thezj <thezj@vip.qq.com>
2017-12-15 16:01:49 +0800

    scale axis simple

---
+++ 1215.js
@@ -0,0 +1,56 @@
+let svg = d3.select('#chart')
+    .append('svg')
+    .attr('width', 500)
+    .attr('height', 500)
+
+let dataset = [10, 20, 30, 40, 33, 24, 12, 5]
+
+let xScale = d3.scaleBand()
+    .domain(d3.range(dataset.length))
+    .range([0, 360])
+
+console.log(xScale.step())
+
+let yScale = d3.scaleLinear()
+    .domain([0, d3.max(dataset)])
+    .range([360, 0])
+
+console.log(yScale(39))
+console.log(yScale(2))
+
+let xAxis = d3.axisBottom(xScale),
+    yAxis = d3.axisLeft(yScale)
+svg.append('g')
+    .attr('class', 'axis')
+    .attr('transform', 'translate(30,380)')
+    .call(xAxis)
+
+svg.append('g')
+    .attr('class', 'axis')
+    .attr('transform', 'translate(30,20)')
+    .call(yAxis)
+
+let rectpadding = 10
+
+let rects = svg.selectAll('.MyRect')
+    .data(dataset)
+    .enter()
+    .append('rect')
+    .attr('class', 'MyRect')
+    .attr('transform', `translate(30,20)`)
+    .attr('x', (d, i) => xScale(i) + rectpadding / 2)
+    .attr('y', (d, i) => yScale(d))
+    .attr('width', xScale.step() - rectpadding)
+    .attr('height', d => 400 - yScale(d) - 40)
+
+let texts = svg.selectAll('.MyText')
+    .data(dataset)
+    .enter()
+    .append('text')
+    .attr('class', 'MyText')
+    .attr('transform', `translate(30,20)`)
+    .attr('x', (d, i) => xScale(i) + rectpadding / 2)
+    .attr('y', (d, i) => yScale(d))
+    .attr('dx', _ => (xScale.step() - rectpadding) / 2)
+    .attr('dy', _ => 20)
+    .text(d => d)
